12f4b3cc5fcb6356da26cdf4dea323a423e5a927
Vedansh Sharma <svedansh99@gmail.com>
2025-04-26 18:53:21 +0530

    "completed fetch_news_data"

--- fetch_news.py
+++ fetch_news.py
@@ -1,11 +1,15 @@
-import pandas
 import requests 
 import datetime
+from datetime import timedelta
+import requests
 from datetime import datetime
 import uuid 
 import json
 import os 
-from google.cloud import storage 
+from google.cloud import storage
+import pandas as pd
+from dotenv import load_dotenv
+load_dotenv()
 
 
 def upload_to_gcs(bucket_name, source_file_name, destination_blob_name):
@@ -27,7 +31,7 @@ def fetch_news_data():
     url = "https://newsapi.org/v2/everything"
     api_key = os.getenv('NEWS_API_KEY')
     query = "technology"
-    from_date = (datetime.now() - datetime.timedelta(days=1)).strftime('%Y-%m-%d')
+    from_date = (datetime.now() - timedelta(days=1)).strftime('%Y-%m-%d')
     to_date = datetime.now().strftime('%Y-%m-%d')
 
     params = {
@@ -43,7 +47,57 @@ def fetch_news_data():
     
     if response.status_code == 200:
         data = response.json()
-        return data['articles']
     else:
         print(f"Error fetching data: {response.status_code}")
-        return []
+        return None 
+
+    df = pd.DataFrame(columns=['newsTitle', 'timestamp', 'url_source', 'content', 'source', 'author', 'urlToImage'])
+
+    for article in data['articles']:
+        news_title = article['title']
+        timestamp = article['publishedAt']
+        url_source = article['url']
+        source = article['source']['name']
+        author = article['author']
+        urlToImage = article['urlToImage']
+    
+        partial_content = article['content'] if article['content'] is not None else ""
+        
+        if len(partial_content) >= 200:
+            trimmed_part = partial_content[:199]
+        if '.' in partial_content:
+            trimmed_part = partial_content[:partial_content.rindex('.')]
+        else:
+            trimmed_part = partial_content
+        
+        new_row = pd.DataFrame({
+            'newsTitle': [news_title],
+            'timestamp': [timestamp],
+            'url_source': [url_source],
+            'content': [trimmed_part],
+            'source': [source],
+            'author': [author],
+            'urlToImage': [urlToImage]
+        })
+
+        df = pd.concat([df, new_row], ignore_index=True)
+    
+    print(df.head())
+    current_time = datetime.now().strftime("%Y%m%d%H%M%S")
+    filename = f'run_{current_time}.parquet'
+
+    #check current working directory
+    print("Current working directory : " , os.getcwd())
+
+    #write to parquet file 
+    df.to_parquet(filename)
+
+    #uplaod to GCS bucket
+    bucket_name = os.getenv('BUCKET_NAME')
+    destination_blob_name = f'news_data_analysis/{filename}'
+    upload_to_gcs(bucket_name, filename, destination_blob_name)
+
+    #remove file from local storage 
+    os.remove(filename)
+
+# fetch_news_data()
